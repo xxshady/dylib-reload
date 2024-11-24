27cabd000a4940d94605ea6f2edf8adea04b4321
xshady <54737754+xxshady@users.noreply.github.com>
2024-11-24 18:46:09 +0300

    fix

--- host/src/helpers.rs
+++ host/src/helpers.rs
@@ -1,7 +1,21 @@
-use std::ffi::CStr;
+use stabby::{libloading::StabbyLibrary, IStable};
 
 pub fn unrecoverable(message: &'static str) -> ! {
   eprintln!("something unrecoverable happened: {message}");
   eprintln!("aborting");
   std::process::abort();
+}
+
+pub fn cstr_bytes(str: &str) -> Vec<u8> {
+  [str.as_bytes(), &[0]].concat()
+}
+
+pub unsafe fn get_stabbied_fn<F>(library: &impl StabbyLibrary, name: &str) -> F
+where
+  F: IStable + Copy,
+{
+  let symbol = library.get_stabbied(&cstr_bytes(name)).unwrap_or_else(|e| {
+    panic!("Failed to get {name} symbol from library, reason: {e:#?}");
+  });
+  *symbol
 }
