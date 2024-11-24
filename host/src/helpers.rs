e740286784cff561e0af9867a145aa3018923771
xshady <54737754+xxshady@users.noreply.github.com>
2024-11-24 18:30:41 +0300

    fix

--- host/src/helpers.rs
+++ host/src/helpers.rs
@@ -10,9 +10,9 @@ pub fn cstr_bytes(str: &str) -> Vec<u8> {
   [str.as_bytes(), &[0]].concat()
 }
 
-pub unsafe fn get_stabbied_fn(library: &impl StabbyLibrary, name: &str) -> impl IStable + Copy {
+pub unsafe fn get_stabbied_fn<F: IStable + Copy>(library: &impl StabbyLibrary, name: &str) -> F {
   let symbol = library.get_stabbied(&cstr_bytes(name)).unwrap_or_else(|e| {
     panic!("Failed to get {name} symbol from library");
   });
-  Ok(*symbol)
+  *symbol
 }
