ae7ba3a55b2d8bf761a5200faaeb871e67a79d7e
xshady <54737754+xxshady@users.noreply.github.com>
2024-11-24 18:47:21 +0300

    fix

--- host/src/lib.rs
+++ host/src/lib.rs
@@ -1,6 +1,6 @@
 use stabby::{libloading::StabbyLibrary, IStable};
 
-pub unsafe fn get_stabbied_fn(library: &impl StabbyLibrary, name: &str) -> impl IStable + Copy {
+pub unsafe fn get_stabbied_fn<F: IStable + Copy>(library: &impl StabbyLibrary, name: &str) -> F {
   let symbol = library.get_stabbied(&[]).unwrap();
   *symbol
 }
