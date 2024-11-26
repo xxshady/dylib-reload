1933ebefc5ae0bac20d7207cf47c46be280d0ebe
xshady <54737754+xxshady@users.noreply.github.com>
2024-11-26 16:52:50 +0300

    second repro

--- host/src/lib.rs
+++ host/src/lib.rs
@@ -1,6 +1,14 @@
 use stabby::{libloading::StabbyLibrary, IStable};
 
-pub unsafe fn get_stabbied_fn<F: IStable + Copy>(library: &impl StabbyLibrary, name: &str) -> F {
-  let symbol = library.get_stabbied(&[]).unwrap();
-  *symbol
+pub unsafe fn get_stabbied_fn<R: IStable>(library: &impl StabbyLibrary) -> R {
+  let symbol: extern "C" fn() -> R = *library.get_stabbied(&[]).unwrap();
+  symbol()
+}
+
+pub fn error() {
+  let library = (|| -> libloading::Library { todo!() })();
+
+  unsafe {
+    get_stabbied_fn(&library);
+  }
 }
