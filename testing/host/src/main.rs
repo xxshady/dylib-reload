eb909f82edd6347d5bd4b2e335783df9cdddd9eb
xshady <xshady@xshady.com>
2024-11-29 22:21:59 +0300

    switch to underscore_names

--- testing/host/src/main.rs
+++ testing/host/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-  let module = unsafe { host::load_module("target/debug/libtest_module.so") }.unwrap();
+  let module = unsafe { dylib_reload_host::load_module("target/debug/libtest_module.so") }.unwrap();
   unsafe {
     // TODO: fix compiler overflow error if turbo fish is removed
     module.main::<()>();
